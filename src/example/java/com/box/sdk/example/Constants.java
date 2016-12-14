2f317042b7f3fa00e44dfe74c741678b1ce912a7
JJ Loneman <jjloneman+sourceforge@gmail.com>
2016-12-14 17:56:53 -0600

    Created Constants class

---
+++ src/example/java/com/box/sdk/example/Constants.java
@@ -0,0 +1,19 @@
+package com.box.sdk.example;
+
+/**
+ * Created by JJ on 12/14/2016.
+ */
+public final class Constants {
+
+    private static final String CLIENT_ID = "";
+    private static final String CLIENT_SECRET = "";
+    private static final String ENTERPRISE_ID = "";
+    private static final String PUBLIC_KEY_ID = "355a7qrb";
+    private static final String PRIVATE_KEY_FILE = "C:\\Users\\JJ\\Documents\\Box.com Test\\private_key.pem";
+    private static final String PRIVATE_KEY_PASSWORD = "";
+    private static final String APP_USER_NAME = "";
+    private static final String DEVELOPER_TOKEN = "";
+    private static final int MAX_CACHE_ENTRIES = 100;
+
+    private Constants() {}
+}
