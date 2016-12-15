390189fdfbe03e5267d26b462b1ef50cebe38341
JJ Loneman <jjloneman+sourceforge@gmail.com>
2016-12-14 18:11:24 -0600

    Updated Constants

--- src/example/java/com/box/sdk/example/Constants.java
+++ src/example/java/com/box/sdk/example/Constants.java
@@ -5,14 +5,14 @@ package com.box.sdk.example;
  */
 public final class Constants {
 
-    public static final String CLIENT_ID = "";
-    public static final String CLIENT_SECRET = "";
+    public static final String CLIENT_ID = "9gj5149b1tmwtdn8kelcq60h9xldikvp";
+    public static final String CLIENT_SECRET = "tOR0ObLlmBSneKrfkGdRHvIOG0cVSlGj";
     public static final String ENTERPRISE_ID = "";
     public static final String PUBLIC_KEY_ID = "355a7qrb";
     public static final String PRIVATE_KEY_FILE = "C:\\Users\\JJ\\Documents\\Box.com Test\\private_key.pem";
     public static final String PRIVATE_KEY_PASSWORD = "";
     public static final String APP_USER_NAME = "";
-    public static final String DEVELOPER_TOKEN = "";
+    public static final String DEVELOPER_TOKEN = "4IZN4ZIEzqOT6gjp9W9gw1dawNrrpamV";
     public static final String USER_ID = "";
 
     public static final int MAX_CACHE_ENTRIES = 100;
