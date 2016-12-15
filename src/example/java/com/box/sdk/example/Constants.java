8f416ba7e519e26c6b267addec8ef77fc9d85852
JJ Loneman <jjloneman+sourceforge@gmail.com>
2016-12-14 19:21:24 -0600

    Edited key files, Constants to reflect new keys, and .gitignore

--- src/example/java/com/box/sdk/example/Constants.java
+++ src/example/java/com/box/sdk/example/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     public static final String CLIENT_ID = "9gj5149b1tmwtdn8kelcq60h9xldikvp";
     public static final String CLIENT_SECRET = "tOR0ObLlmBSneKrfkGdRHvIOG0cVSlGj";
     public static final String ENTERPRISE_ID = "";
-    public static final String PUBLIC_KEY_ID = "355a7qrb";
+    public static final String PUBLIC_KEY_ID = "4iaxqmyd";
     public static final String PRIVATE_KEY_FILE = "src/main/resources/private_key.pem";
     public static final String PRIVATE_KEY_PASSWORD = "pass";
     public static final String APP_USER_NAME = "Client";
